ead9e733f81510f12eca75027e23aa68f923b8b2
Kevin Adhaikal <109958650+KevinAdhaikal@users.noreply.github.com>
2024-09-08 14:30:04 +0700

    Update and rename example.js to example.cjs

--- example.cjs
+++ example.cjs
@@ -1,8 +1,6 @@
-const CAINode = require("./index.js");
-const Char_AI = new CAINode();
-
 (async function() {
+    const Char_AI = (await import("./index.js")).CAINode();
     await Char_AI.login("Your Character AI Token");
     console.log("Login successfully");
-    await Char_AI.logout()
+    await Char_AI.logout();
 })();
